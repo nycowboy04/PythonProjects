16b82850f4782738163732c0e767552fb2580b22
Matthew Sheinberg <msheinberg@gmail.com>
2018-03-15 16:54:01 -0400

    Beginning implementation of connect4 project. Coded the HTML, linked bootstrap3 and jQuery. Set up the basic CSS.

---
+++ Connect4Project/connect4.js
@@ -0,0 +1,9 @@
+Player1=input("Player 1, please enter your name. Your color is blue. ");
+Player2=input('Player 2, please enter your name. Your color is red. ');
+
+var turn = "p1";
+var winner = false;
+
+while (winner===false) {
+  
+}
