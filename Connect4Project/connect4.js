ee88b4ab2b316e693b8cae87539ef539c0485e75
Matthew Sheinberg <msheinberg@gmail.com>
2018-03-19 14:25:54 -0400

    working on js file

--- Connect4Project/connect4.js
+++ Connect4Project/connect4.js
@@ -1,9 +1,64 @@
-Player1=input("Player 1, please enter your name. Your color is blue. ");
-Player2=input('Player 2, please enter your name. Your color is red. ');
+var Player1=prompt("Player 1, please enter your name. Your color is blue. ");
+var player1Color='rgb(86, 151, 255)';
 
-var turn = "p1";
-var winner = false;
+var Player2=prompt('Player 2, please enter your name. Your color is red. ');
+var player2Color='rgb(237, 45, 73)';
 
-while (winner===false) {
-  
+var game_on = true;
+var table = $('table tr');
+
+function reportWin(rowNum, colNum){
+  console.log("You won starting at this row,col");
+  console.log(rowNum);
+  console.log(colNum);
+}
+
+function changeColor(rowIndex,colIndex, color) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color',color)
+}
+
+function returnColor(rowIndex,colIndex, color) {
+  return table.eq(rowIndex).find('td').eq(colIndex).find('button').css('background-color');
+}
+
+function checkBottom() {
+  var colorReport = returnColor(5, colIndex);
+  for (var row = 0, row > -1, row--){
+    colorReport=(row, colIndex);
+    if(colorReport==='rgb(128, 128 ,128)'){
+      return row;
+    }
+  }
+}
+
+function colorMatchCheck(one,two,three,four) {
+  return (one===two && one===three && one===four && one!=='rgb(128, 12, 128)' && one !== undefined)
+}
+
+function horizontalWinCheck() {
+  for (var row=0;row<6;row++){
+    for (var col = 0; col < 4;coli++) {
+      if(colorMatchCheck(returnColor(row,col), returnColor(row,col+1), returnColor(row,col+2), returnColor(row, col+3))){
+        console.log('horiz');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
+
+function verticalWinCheck() {
+  for(var col = 0, col<7, col++){
+    for(var row = 0, row < 3, row++){
+      if (colorMatchCheck(returnColor(row,col),returnColor(row+1, col), returnColor(row+2,col), returnColor(row+3,col))) {
+          console.log('vertical');
+          reportWin(row,col);
+          return true;
+      }else {
+        continue;
+      }
+    }
+  }
 }
