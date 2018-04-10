b16e5f0d66ccf6a2b91aa0c3e3ed3c6a0153ef46
Matthew Sheinberg <msheinberg@gmail.com>
2018-04-10 17:37:04 -0400

    Forgot to add these projects into the overall history

---
+++ Python-Django-Course-Udemy/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matthewsheinberg/Repos/ProgrammingPractice/Python-Django-Course-Udemy/myvenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
